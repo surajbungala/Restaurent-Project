f62b8d5a1e5c82c8117911743dbaae0904ef2289
Suraj Bungala <surajbungala127@gmail.com>
2022-02-19 21:31:15 +0530

    Initial and Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponents';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante conFusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorante conFusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes = {this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
