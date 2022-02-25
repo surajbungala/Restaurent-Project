86b5b4978cfa36d7608fa011ff438d1ad099a158
surajbungala <99757138+surajbungala@users.noreply.github.com>
2022-02-25 13:05:35 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import './App.css';
 import { DISHES } from './shared/dishes';
 import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component{
   constructor(props){
     super(props);
     this.state = {
       dishes : DISHES
     };
   }
-  {
+  
+    render(){
     return (
       <div className="App">
         <Navbar dark color="primary">
